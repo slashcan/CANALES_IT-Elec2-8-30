cf1bb95e571eeaa80ce042e5343a0ca36f6e1659
slashcan <slashc170@gmail.com>
2023-03-18 12:48:02 +0800

    Actionbar 18/03/2023

---
+++ app/src/main/java/com/example/helloandroid/actionbar.java
@@ -0,0 +1,77 @@
+package com.example.helloandroid;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+import androidx.fragment.app.DialogFragment;
+
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class actionbar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actionbar);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setTitle("GfG | Action Bar");
+
+        actionBar.setSubtitle("Design a customer Action Bar");
+
+        actionBar.setIcon(R.mipmap.ic_launcher);
+
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.search:
+                showAlertDialog();
+                break;
+            case R.id.refresh:
+                DialogFragment dialogFragment = new DialogFragment();
+                dialogFragment.show(getSupportFragmentManager(),"My Fragment");
+                break;
+            case R.id.copy:
+                Toast.makeText(this, "Copy Clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showAlertDialog()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Alert!")
+                .setMessage("Danger you are failing!");
+        builder.setPositiveButton("I know!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
