83ebba582b0f6be27faa3fc75358be50562071a6
slashcan <slashc170@gmail.com>
2023-04-15 12:27:43 +0800

    15/4/2023 - bottomnav

---
+++ app/bottomnav/src/main/java/com/example/bottomnav/ui/home/HomeFragment.java
@@ -0,0 +1,53 @@
+package com.example.bottomnav.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.bottomnav.Secondactivity;
+import com.example.bottomnav.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        final Button buttonGotoSecond = binding.button;
+
+        buttonGotoSecond.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getContext(), Secondactivity.class);
+                intent.putExtra("PASS_ME", "Hi im from second activity");
+                startActivity(intent);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/bottomnav/src/main/java/com/example/bottomnav/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,112 @@
+package com.example.bottomnav.ui.notifications;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.bottomnav.MainActivity;
+import com.example.bottomnav.databinding.FragmentNotificationsBinding;
+
+public class NotificationsFragment extends Fragment {
+
+    private FragmentNotificationsBinding binding;
+    
+    IntentFilter intentfilter;
+    
+    int deviceHealth;
+    
+    String currentBatteryHealth = "Battery Health";
+    
+    int batteryLevel;
+    
+    TextView textView;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        NotificationsViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(NotificationsViewModel.class);
+
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        textView = binding.textNotifications;
+        final Button checkStatus = binding.button3;
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+        intentfilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+
+        checkStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getActivity().registerReceiver(broadcastreceiver, intentfilter);
+            }
+        });
+        return root;
+    }
+
+    private BroadcastReceiver broadcastreceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            deviceHealth = intent.getIntExtra(BatteryManager.EXTRA_HEALTH,0);
+
+            
+            if (deviceHealth == BatteryManager.BATTERY_HEALTH_COLD)
+            {
+                textView.setText(currentBatteryHealth+" = cold");
+            }
+            
+            if (deviceHealth == BatteryManager.BATTERY_HEALTH_DEAD)
+            {
+                textView.setText(currentBatteryHealth+" = dead");
+            }
+
+            if (deviceHealth == BatteryManager.BATTERY_HEALTH_GOOD)
+            {
+                textView.setText(currentBatteryHealth+" = good");
+            }
+
+            if (deviceHealth == BatteryManager.BATTERY_HEALTH_OVERHEAT)
+            {
+                textView.setText(currentBatteryHealth+" = overheat");
+            }
+
+            if (deviceHealth == BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE)
+            {
+                textView.setText(currentBatteryHealth+" = over");
+            }
+
+            if (deviceHealth == BatteryManager.BATTERY_HEALTH_UNKNOWN)
+            {
+                textView.setText(currentBatteryHealth+="N/A");
+            }
+
+            if (deviceHealth == BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE)
+            {
+                textView.setText(currentBatteryHealth+="Failure");
+            }
+
+        }
+    };
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
