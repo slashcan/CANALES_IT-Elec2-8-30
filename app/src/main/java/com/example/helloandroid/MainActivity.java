b34261d204f03ba768413c72e145567cc72a5384
slashcan <slashc170@gmail.com>
2023-04-29 11:49:21 +0800

    29042023 - SQLite

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.helloandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private MyDatabaseHelper dbHelper;
 
     private static final String TAG = "MainActivity";
     @Override
@@ -17,21 +21,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.v(TAG,"This is a verbose log.");
-        Log.d(TAG,"This is a debug log.");
-        Log.i(TAG,"This is an info log.");
-        Log.w(TAG, "This is a warn log.");
-        Log.e(TAG,"This is an error log");
+        dbHelper = new MyDatabaseHelper(this);
 
-        Button button = (Button) findViewById(R.id.button6);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               Log.i(TAG,"Button Clicked");
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
-            }
-        });
+        dbHelper.deleteAllData();
+
+        dbHelper.insertData("John", 25);
+        dbHelper.insertData("Luke", 24);
+
+        getData();
+
+        dbHelper.updateData(2, "Mark", 26);
+
+        getData();
+
+        dbHelper.deleteData(2);
+
+        getData();
+
+        SharedPreferences prefs = getSharedPreferences("my_prefs", MODE_PRIVATE);
+
+
 
 
     }
